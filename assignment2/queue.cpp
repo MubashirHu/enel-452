9137c41f34bb73b058249943ab4611e3ada5ec1a
MubashirHu <hussainmuby@gmail.com>
2023-10-16 06:51:31 -0600

    Implementation for node insertion

--- assignment2/queue.cpp
+++ assignment2/queue.cpp
@@ -61,10 +61,83 @@ void Queue::insert(Data d)
     nelements++;
 }
 
-
+/** 
+     Insert a new element at a specific position of the queue 
+     \post Data element will be inserted IN FRONT of the given position
+     If the position is 0,               data element will be inserted at the front of the queue(the head)
+     If the position is set to size(),   data element will be appended to the tail
+     If the position is set to size()-1, data element will insert just in front of the tail
+    */
 void Queue::insert(Data d, unsigned position)
 {
-   
+    if(position == 0)
+    {
+        QElement* el = new QElement(d);
+        if(size() > 0)
+        {
+            el->next = head;
+            head = el;
+        }
+        else
+        {
+            head = el;
+            tail = el;
+        }
+        nelements++;
+    }
+    else if(position == size())
+    {
+        insert(d);
+    }
+    else if(position == (size()-1))
+    {
+        QElement* el = new QElement(d);
+        QElement* current;
+        if(size() > 0)
+        {
+            current = head;
+            while(current->next != tail)
+            {
+                current = current->next;
+            }
+
+            //current has reached right before the tail
+            el->next = current->next;
+            current->next = el;
+        }
+        else
+        {
+            head = el;
+            tail = el;
+        }
+        nelements++;
+    }
+    else if(position > 0 && (position < (size()-1)))
+    {
+        QElement* el = new QElement(d);
+        QElement* current = head; // Initialize 'current' to the head of the list
+        unsigned int i = 0;
+
+        if (size() > 0)
+        {
+            while (current->next != NULL && i < (position - 1))
+            {
+                if (i == position)
+                {
+                    el->next = current->next;
+                    current->next = el;
+                    break; // No need to continue iterating
+                }
+                current = current->next;
+                i++;
+            }
+        }
+        else
+        {
+            head = el;
+            tail = el;
+        }
+    }
 }
 
 bool Queue::search(Data otherData) const
