fdba0d9cd916eecfa6275e32a80d27d277abe42a
Mubashir <hussainmuby@gmail.com>
2023-10-17 10:36:34 -0600

    added new prototype for sendbyte() to include timeout

--- lab3/headers/USART.h
+++ lab3/headers/USART.h
@@ -21,20 +21,22 @@ extern volatile uint8_t dataReceivedFlag; // Declaration, no initialization
 
 /** Configure and enable the device. */
 void serial_open(void);
+
 /**
 Undo whatever serial_open() did, setting the peripheral back to
 its reset configuration.
 2
 */
 void serial_close(void);
+
 /**
-Send an 8-bit byte to the serial port, using the configured
-bit-rate, # of bits, etc. Returns 0 on success and non-zero on
-failure.
+Send an 8-bit byte to the serial port, using the configured bit-rate, # of bits, etc.
+Returns 0 on success and non-zero on failure.
 @param b the 8-bit quantity to be sent.
 @pre must have already called serial_open()
 */
-void sendbyte(char b);
+int sendbyte(uint8_t b, uint32_t Timeout);
+
 /**
 Gets an 8-bit character from the serial port, and returns it.
 @pre must have already called serial_open()
