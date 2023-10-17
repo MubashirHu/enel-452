d84c53d9c3392bbaf41f8788824a273979f7da58
Mubashir <hussainmuby@gmail.com>
2023-10-17 09:30:00 -0600

    USART2 interrupt handler implementation #1

--- lab3/src/USART.c
+++ lab3/src/USART.c
@@ -88,3 +88,14 @@ char getbyte(void)
 	
 		return USART2->DR;
 }
+
+void USART2_IRQHandler(void) {
+    if ((USART2->SR & USART_SR_RXNE) != 0) {
+        uint8_t receivedData = USART2->DR; // Read the received data
+			
+        // Process the receivedData
+				sendbyte(receivedData);
+			
+        dataReceivedFlag = 1; // Set the flag to indicate character reception
+    }
+}
