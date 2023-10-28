99d79dd281e3fc506ce988029c4f082bb797c8df
Mubashir <hussainmuby@gmail.com>
2023-10-28 10:46:24 -0600

    got Blinky to work

--- lab5/src/main.c
+++ lab5/src/main.c
@@ -17,27 +17,31 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "stm32f10x.h"
+#define BLINKY_TASK_PRIORITY 5
 
 volatile uint8_t DATA_RECEIVED_FLAG = 0; // Global declaration and initialization
 volatile uint8_t TIM3_UPDATE_EVENT = 0;
+
+static void vBlinkTask(void * parameters);
 
 int main(void)
 {
 	clockInit();
 	serialOpen();
-	prepareTerminal();
+	//prepareTerminal();
 	initUSART2Interrupt();
 	ledIOInit();
 	
-	initTIM(2);
-	initTIM(3);
-	configTIM(3, 1000);
-	initTIMInterrupt(3);
+	//initTIM(2);
+	//initTIM(3);
+	//configTIM(3, 1000);
+	//initTIMInterrupt(3);
 
-	uint8_t buffer[512];
+	//uint8_t buffer[512];
 	
-	int bufferElementID = 0;
+	//int bufferElementID = 0;
 	
+	/*
 	while(1)
 	{
 		if(DATA_RECEIVED_FLAG == 1)
@@ -52,6 +56,11 @@ int main(void)
 			TIM3_UPDATE_EVENT = 0;
 		}
 	}
+	*/
+	
+	xTaskCreate(vBlinkTask, "Blinky", configMINIMAL_STACK_SIZE+10, NULL, BLINKY_TASK_PRIORITY, NULL);  
+	 
+	vTaskStartScheduler();
 }
 
 void USART2_IRQHandler(void) {
@@ -62,4 +71,15 @@ void USART2_IRQHandler(void) {
 void TIM3_IRQHandler(void) {
 	TIM3_UPDATE_EVENT = 1;
 	TIM3->SR &= ~(TIM_SR_UIF); // reset update event flag
+}
+
+static void vBlinkTask(void * parameters) {
+	
+	while(1)
+	{
+	onboardLEDconfig(1);
+	vTaskDelay(1000);
+	onboardLEDconfig(0);
+	vTaskDelay(1000);
+	}
 }
