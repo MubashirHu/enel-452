df059ac47393a7b2e0fc75a1adfaf291b7af35ef
Mubashir <hussainmuby@gmail.com>
2023-10-28 13:45:21 -0600

    Fixed an error with cursor positioning when Parsing

--- lab5/src/CLI.c
+++ lab5/src/CLI.c
@@ -79,13 +79,15 @@ int parseReceivedData(uint8_t *pData, int Size)
 	{
 		uint8_t buffer[] = "On-board led is ON";
 		CLI_Transmit(buffer, sizeof(buffer));
-		onboardLEDconfig(1);		
+		onboardLEDconfig(1);
+		return 0;
 	} 
 	else if(strncmp((char*)pData, "ledoff\r", 7) == 0)
 	{
 		uint8_t buffer[] = "On-board led is OFF";
 		CLI_Transmit(buffer, sizeof(buffer));
 		onboardLEDconfig(0);
+		return 0;
 	}
 	else if(strncmp((char*)pData, "ledstate\r", 8) == 0)
 	{
@@ -93,27 +95,31 @@ int parseReceivedData(uint8_t *pData, int Size)
 		{
 			uint8_t buffer[] = "led is on";
 			CLI_Transmit(buffer, sizeof(buffer));
+			return 0;
 		}
 		else 
 		{
 			uint8_t buffer[] = "led is off";
 			CLI_Transmit(buffer, sizeof(buffer));
+			return 0;
 		}
 	}
 	else if(strncmp((char*)pData, "help\r", 5) == 0)
 	{
 		uint8_t buffer[] = "Help command Currently the commands available are 'ledon', 'ledoff', 'ledstate'. If a command is typed incorrectly an error prompt of 'invalid command' will show up. If the wrong command is typed, backspacing is available";
 		CLI_Transmit(buffer, sizeof(buffer));
+		return 0;
 	}else if(strncmp((char*)pData, "clear\r", 6) == 0)
 	{
 		sendEscapeAnsi(CLEAR_TERMINAL);
-		return 1;
+		return 0;
 	}
 	else
 	{
 		uint8_t buffer[] = "Unknown command:";
 		CLI_Transmit(buffer, sizeof(buffer));
 		CLI_Transmit(pData, Size);
+		return 0;
 	}
 }
 
