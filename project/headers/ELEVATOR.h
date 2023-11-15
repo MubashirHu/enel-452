44ec37239e535a1f60dd8d003aa167bd36ed77ec
MubashirHu <hussainmuby@gmail.com>
2023-11-14 21:19:39 -0600

    completed comments for header files

--- project/headers/ELEVATOR.h
+++ project/headers/ELEVATOR.h
@@ -66,19 +66,128 @@ typedef struct
 	enum maintenanceMode maintenanceStatus;
 }ElevatorInformation;
 
+/**
+ * @brief Initializes GPIO pins for elevator control.
+ *
+ * This function initializes the GPIO pins required for elevator control.
+ * It configures the necessary hardware resources to set up the elevator system.
+ */
 void initGPIOPinsForElevator(void);
+
+/**
+ * @brief Sets the LED corresponding to the specified floor on the MUX.
+ *
+ * This function turns on the LED indicator for the given floor to indicate the current position of
+ * the elevator.
+ *
+ * @param 'floor' The floor for which the LED should be set.
+ */
 void setLED(int floor);
+
+/**
+ * @brief Processes upward elevator requests.
+ *
+ * This function processes requests for the elevator to move up, based on the provided elevator information.
+ * Only ever called when the direction of the elevator is UP.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void processUpRequests(ElevatorInformation *elevator);
+
+/**
+ * @brief Processes downward elevator requests.
+ *
+ * This function processes requests for the elevator to move down, based on the provided elevator information.
+ * Only ever called when the direction of the elevator is DOWN.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void processDownRequests(ElevatorInformation *elevator);
+
+/**
+ * @brief Returns the elevator to its home (FIRST) floor.
+ *
+ * This function instructs the elevator to return to its initial home position.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void goHome(ElevatorInformation *elevator);
+
+/**
+ * @brief Moves the elevator to the upper floor.
+ *
+ * This function increments the current floor. 
+ * Called when trying to reach a target floor.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void moveToUpperFloor(ElevatorInformation *elevator);
+
+/**
+ * @brief Moves the elevator to the lower floor.
+ *
+ * This function increments the current floor. 
+ * Called when trying to reach a target floor.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void moveToLowerFloor(ElevatorInformation *elevator);
+
+/**
+ * @brief Updates the LCD display
+ *
+ * This function updates the LCD display with the current floor information
+ * and the direction that the elevator is headed in.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void updateLCDToNewFloor(ElevatorInformation *elevator);
+
+/**
+ * @brief Determines the direction of the elevator.
+ *
+ * This function determines the direction (up, down, or stationary(idle)) of the elevator 
+ * based on its current state. This state is dependent on whether there are any requests or not. 
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void determineElevatorDirection(ElevatorInformation *elevator);
+
+/**
+ * @brief Handles messages for passengers entering the elevator.
+ *
+ * This function displays messages and instructions for passengers entering the elevator
+ * through the LCD.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void messagesEnteringElevator(ElevatorInformation *elevator);
+
+/**
+ * @brief Handles messages for passengers leaving the elevator.
+ *
+ * This function displays messages and instructions for passengers leaving the elevator
+ * through the LCD.
+ *
+ * @param '*elevator' A pointer to the ElevatorInformation structure.
+ */
 void messagesLeavingElevator(ElevatorInformation *elevator);
+
+/**
+ * @brief Initializes the PB8 as an interrupt. 
+ *
+ * @param None
+ */
+void initMaintenanceButtonInterrupt(void);
+
+/**
+ * @brief Interrupt handler prototype
+ *
+ * @param None
+ */
+void EXTI9_5_IRQHandler(void);
+
+//test - not used
 void readElevator(void);
 
-void initMaintenanceButtonInterrupt(void);
-void EXTI9_5_IRQHandler(void);
 #endif //ELEVATOR_H
--- project/headers/TIM.h
+++ project/headers/TIM.h
@@ -1,11 +1,9 @@
 /**
  * @file TIM.h
  *
- * @brief USART2 Configuration for STM32 Communication with Host Terminal.
+ * @brief Timer Configuration for STM32
  *
- * This header file enables the USART2 port, which is used for communication between
- * an STM32 board and a host terminal. It utilizes the same communication interface
- * as the ST-Link debugger. The file also includes functions previously used in ENEL 351.
+ * This file contains the prototypes for timer initialization and handlers. 
  *
  * @author Mubashir Hussain
  * @studentID 200396797
@@ -16,12 +14,12 @@
 #include <stdint.h>
 #define SENDDATA_TIMER 10
 #define RECEIVEDATA_TIMER 10
-extern volatile uint8_t TIM3_UPDATE_EVENT; // Declaration, no initialization
-extern volatile uint8_t TIM4_UPDATE_EVENT; // Declaration, no initialization
+extern volatile uint8_t TIM3_UPDATE_EVENT; 
+extern volatile uint8_t TIM4_UPDATE_EVENT;
 
 /**
-Initialize the general purpose timer TIM2
-Overall, this initialization function configures TIM2 to count at a rate of 10 kHz 
+Initialize the general purpose timer. accepts timer 2, 3 and 4. 
+Overall, this initialization function configures timer to count at a rate of 10 kHz 
 (0.1 ms per count), with a period of 1 ms. The timer is set to start counting from 0 
 and will generate an update event every 1 ms.
 */
